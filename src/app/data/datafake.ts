c1eb20b39a13fa64b6dafb28e4ad25fde43d5cca
dreeilima <dreiiziin@gmail.com>
2023-10-13 22:52:30 -0300

    content creat

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVO FILME DO HOMEM DE FERRO TE ESPERA",
+        "description": "Marvel anuncia novo filme extraordinario",
+        "photoCover": "https://picsum.photos/300/200"
+    },
+    {
+        "id": "2",
+        "title": "saiu novo filme no Disney+",
+        "description": "veja o novo filme sem esperar.",
+        "photoCover": "https://picsum.photos/id/2/200"
+    }
+]
